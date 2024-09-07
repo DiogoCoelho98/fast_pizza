5e31c01883b9e094ac49677cc7bf26705f449a02
Diogo Coelho <diogocoelho19988@gmail.com>
2024-09-07 08:45:43 +0100

    createBrowserRouter implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { createBrowserRouter, RouterProvider  } from "react-router-dom"
+
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
+import Cart from "./features/cart/Cart.jsx";
+import CreateOrder from "./features/order/CreateOrder.jsx";
+import Order from "./features/order/Order.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/menu",
+    element: <Menu/>
+  },
+  {
+    path: "/cart",
+    element: <Cart/>
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder/>
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order/>
+  }
+]);
+
 export default function App() {
   return (
     <>
-      <h1>test</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11,7 +11,7 @@ export function formatDate(date){
     return new Intl.DateTimeFormat("en", {
         day: "numeric",
         month: "short",
-        hour: "2-digit";
+        hour: "2-digit",
         minute: "2-digit"
     }).format(new Date(date));
 }
